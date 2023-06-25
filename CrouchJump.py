8008b411013c7ebfa6285641376ba813c29a57e1
zasbu <136494048+zasbu@users.noreply.github.com>
2023-06-25 13:36:36 +1000

    - Added ability to change +duck key

--- CrouchJump.py
+++ CrouchJump.py
@@ -16,17 +16,20 @@ ctrl_pressed = False
 release_delay = 0.85  # Default release delay in seconds
 paused = False
 pause_hotkey = "enter"  # Default pause/unpause hotkey
+duck_key = "left ctrl"  # Default +duck key
 
 def release_ctrl():
     global ctrl_pressed, release_delay
     time.sleep(release_delay)
     if ctrl_pressed:
-        keyboard.release('left ctrl')
+        keyboard.release(duck_key)
         ctrl_pressed = False
 
 def on_space_press(event):
-    global ctrl_pressed, paused
+    global ctrl_pressed, paused, duck_key
     if paused:
+        if event.name == 'space' or event.name == duck_key:
+            keyboard.release(event.name)
         return
 
     if event.event_type == keyboard.KEY_DOWN:
@@ -34,7 +37,7 @@ def on_space_press(event):
             if keyboard.is_pressed('w'):
                 keyboard.release('w')
             keyboard.press('space')
-            keyboard.press('left ctrl')
+            keyboard.press(duck_key)
             ctrl_pressed = True
             threading.Thread(target=release_ctrl).start()
     elif event.event_type == keyboard.KEY_UP:
@@ -50,8 +53,8 @@ def toggle_pause():
             if paused:
                 if keyboard.is_pressed('space'):
                     keyboard.release('space')
-                if keyboard.is_pressed('left ctrl'):
-                    keyboard.release('left ctrl')
+                if keyboard.is_pressed(duck_key):
+                    keyboard.release(duck_key)
                 paused = False
                 print(Fore.YELLOW + Style.BRIGHT + 'Script unpaused.' + Style.RESET_ALL)
             else:
@@ -63,6 +66,12 @@ def toggle_pause():
         if window_title.endswith('.exe') or 'Counter-Strike' in window_title or 'Momentum' in window_title:
             paused = not paused
             print(Fore.YELLOW + Style.BRIGHT + 'Script ' + ('paused.' if paused else 'unpaused.') + Style.RESET_ALL)
+
+def release_enter(event):
+    if event.name == 'enter':
+        keyboard.release('enter')
+
+keyboard.on_press(release_enter)
 
 # Load configuration from file
 config = configparser.ConfigParser(comment_prefixes=';', allow_no_value=True)
@@ -76,12 +85,15 @@ if os.path.exists(config_file):
             release_delay = float(config['Settings']['release_delay'])
         if 'pause_hotkey' in config['Settings']:
             pause_hotkey = config['Settings']['pause_hotkey']
+        if 'duck_key' in config['Settings']:
+            duck_key = config['Settings']['duck_key']
         if 'first_launch' in config['Settings']:
             first_launch = config.getboolean('Settings', 'first_launch')
 else:
     config.add_section('Settings')
     config.set('Settings', 'release_delay', str(release_delay))
     config.set('Settings', 'pause_hotkey', pause_hotkey)
+    config.set('Settings', 'duck_key', duck_key)
     config.set('Settings', 'first_launch', 'True')
 
     with open(config_file, 'w') as configfile:
@@ -98,6 +110,7 @@ if first_launch:
 
 print(Fore.RED + "Crouch Jump activated. " + Fore.WHITE + Style.BRIGHT + "The script will automatically let go of crouch for you after a specified delay." + Style.RESET_ALL)
 print(Fore.WHITE + Style.BRIGHT + "You can change the uncrouch timing in the " + Fore.BLUE + Style.BRIGHT + "config.ini " + Fore.WHITE + Style.BRIGHT + "file. The default value is " + Fore.BLUE + Style.BRIGHT + "0.85" + Style.RESET_ALL + Fore.WHITE + Style.BRIGHT + " seconds." + Style.RESET_ALL)
+print(Fore.WHITE + Style.BRIGHT + "You can change the " + Fore.BLUE + Style.BRIGHT + "+duck " + Fore.WHITE + Style.BRIGHT +  "key in the " + Fore.BLUE + Style.BRIGHT + "config.ini " + Fore.WHITE + Style.BRIGHT + "file. The default value is " + Fore.BLUE + Style.BRIGHT + "left ctrl." + Style.RESET_ALL)
 print(Fore.YELLOW + Style.BRIGHT + f"Press {Fore.GREEN}{Style.BRIGHT}{pause_hotkey.capitalize()}{Fore.YELLOW}{Style.BRIGHT} to toggle pause/unpause." + Style.RESET_ALL)
 
 keyboard.add_hotkey(pause_hotkey, toggle_pause, suppress=True)
